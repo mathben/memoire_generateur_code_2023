3528629faf6f57e43b55d0b921d5c67404812985
Mathieu Benoit <mathben@technolibre.ca>
2023-02-28 03:05:21 -0500

    [UPD] utiliser

--- exemple_8_generateur_generateur_libre_code_python/main.py
+++ exemple_8_generateur_generateur_libre_code_python/main.py
@@ -3,7 +3,16 @@ import re
 import shutil
 import subprocess
 
+
+def generator_generator(s_code):
+    s_code = s_code.replace("\n", "\\n")
+    code = f'with open("{filename}", "w") as f:\n'
+    code += f"""    f.write('{s_code}')\n"""
+    return code
+
+
 # # Copier
+filename_generateur = "generateur.py"
 filename = "C.py"
 dirname = os.path.dirname(__file__)
 # Incrémentation du chiffre sur le nom du répertoire de destination
@@ -15,30 +24,22 @@ dirname_to = os.path.join(
         os.path.basename(os.path.dirname(__file__)),
     ),
 )
+filename_to = os.path.join(dirname_to, filename)
+filename_generateur_to = os.path.join(dirname_to, filename_generateur)
 print(f"Copié '{dirname_to}'")
 shutil.copytree(dirname, dirname_to, dirs_exist_ok=True)
 # # Étudier
 print("Étudier")
 # ## Lire
-filename_to = os.path.join(dirname_to, filename)
 with open(filename_to, "r") as f:
     code = f.read()
-
-
-def generator_generator(s_code):
-    s_code = s_code.replace("\n", "\\n")
-    code = f'with open("{filename}", "w") as f:\n'
-    code += f"""    f.write('{s_code}')\n"""
-    return code
-
-
 # # Modifier
 print("Modifier")
-with open("generateur.py", "w") as f:
+with open(filename_generateur, "w") as f:
     f.write(generator_generator(code))
-
 subprocess.run(["black", dirname_to], check=True)
-
 # # Utiliser
 print("Utiliser")
-subprocess.run(["python", "generateur.py"], cwd=dirname_to, check=True)
+subprocess.run(["python", filename_generateur_to], cwd=dirname_to, check=True)
+output = subprocess.check_output(["python", filename_to], cwd=dirname_to)
+print(output.decode("utf-8"))
