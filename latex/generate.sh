fed5325173613772c3595ba93218ca2be9e6047e
Mathieu Benoit <mathben@technolibre.ca>
2023-03-17 16:31:12 -0400

    [ADD] latex: script generate

---
+++ latex/generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sed -i 's/\\usepackage{\\xcolor}/%\\usepackage{\\xcolor}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+sed -i 's/\\pagecolor\[rgb\]{0,0,0}/%\\pagecolor\[rgb\]{0,0,0}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+sed -i 's/\\color\[rgb\]{0.5,0.5,0.5}/%\\color\[rgb\]{0.5,0.5,0.5}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+
+cd "Mémoire robot codeur Libre sur ERPLibre" || exit
+make
+
+cd - || exit
+
+sed -i 's/%\\usepackage{\\xcolor}/\\usepackage{\\xcolor}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+sed -i 's/%\\pagecolor\[rgb\]{0,0,0}/\\pagecolor\[rgb\]{0,0,0}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+sed -i 's/%\\color\[rgb\]{0.5,0.5,0.5}/\\color\[rgb\]{0.5,0.5,0.5}/g' "Mémoire robot codeur Libre sur ERPLibre/Document.tex"
+
+mv "Mémoire robot codeur Libre sur ERPLibre/Document.pdf" ./memoire_robot_codeur_libre_erplibre.pdf
+
+xdg-open "./memoire_robot_codeur_libre_erplibre.pdf"
