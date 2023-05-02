e25bc355e36235d99873f89fa43f3f6bed2ddcdb
Mathieu Benoit <mathben@technolibre.ca>
2023-05-02 18:16:35 -0400

    [UPD] presentation: refactoring support multiple model

--- presentation/js/custom.js
+++ presentation/js/custom.js
@@ -18,96 +18,7 @@ function send_terminal(str) {
     iframe.contentWindow.postMessage(message, url);
 }
 
-document.getElementById("demo_1_btn_login").addEventListener("click", function () {
-    console.debug("login");
-    // login
-    send_terminal("mathben");
-
-    // password
-    send_terminal("testtest");
-
-    // clear
-    send_terminal("clear");
-});
-
-document.getElementById("demo_1_btn_run").addEventListener("click", function () {
-    console.debug("run");
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // run
-    send_terminal("./run.sh");
-});
-
-document.getElementById("demo_1_btn_stop").addEventListener("click", function () {
-    console.debug("Kill all");
-    // force kill all
-    send_terminal("pkill -f odoo-bin");
-});
-
-document.getElementById("demo_1_btn_status").addEventListener("click", function () {
-    console.debug("status");
-
-    // cd
-    send_terminal(`cd ${path_repo}`);
-
-    // status
-    send_terminal("git status");
-});
-
-document.getElementById("demo_1_btn_diff").addEventListener("click", function () {
-    console.debug("diff");
-
-    // cd
-    send_terminal(`cd ${path_repo}`);
-
-    // diff
-    send_terminal("git diff");
-});
-
-document.getElementById("demo_1_btn_commit").addEventListener("click", function () {
-    console.debug("commit");
-
-    // cd
-    send_terminal(`cd ${path_repo}`);
-
-    // add
-    send_terminal("git add .");
-
-    // commit
-    send_terminal('git commit -m "test"');
-});
-
-document.getElementById("demo_1_btn_clean").addEventListener("click", function () {
-    console.debug("clean");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        all_good = false;
-    }
-
-    // cd
-    send_terminal(`cd ${path_repo}`);
-
-    // clean
-    send_terminal("git clean -fd");
-
-    if (!all_good) {
-        return;
-    }
-
-    // force clean if exist (or good ;-))
-    send_terminal(`rm -rf "${input_module.value}"`);
-    send_terminal(`rm -rf "code_generator_template_${input_module.value}"`);
-    send_terminal(`rm -rf "code_generator_${input_module.value}"`);
-});
-
-document.getElementById("demo_1_btn_stat_c").addEventListener("click", function () {
-    console.debug("stat c");
-
+function validate_module() {
     let input_module = document.getElementById("demo_1_input_module");
     let all_good = true;
 
@@ -117,213 +28,8 @@ document.getElementById("demo_1_btn_stat_c").addEventListener("click", function 
     } else {
         input_module.classList.remove("invalid");
     }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // stat
-    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_stat_uca").addEventListener("click", function () {
-    console.debug("stat uca");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // stat
-    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_template_${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_stat_ucb").addEventListener("click", function () {
-    console.debug("stat ucb");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // stat
-    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_install").addEventListener("click", function () {
-    console.debug("install");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // restore
-    send_terminal(`./script/database/db_restore.py --database ${bd_name}`);
-
-    // install
-    send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_update").addEventListener("click", function () {
-    console.debug("update");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // update
-    send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_up_uca").addEventListener("click", function () {
-    console.debug("up uca");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // restore
-    send_terminal(`./script/database/db_restore.py --database cg_uca_${bd_name}`);
-
-    // install
-    send_terminal(`./script/addons/install_addons_dev.sh cg_uca_${bd_name} ${input_module.value},code_generator_template_${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_up_ucb").addEventListener("click", function () {
-    console.debug("up ucb");
-
-    let input_module = document.getElementById("demo_1_input_module");
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-    if (!all_good) {
-        return;
-    }
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // restore
-    send_terminal(`./script/database/db_restore.py --database cg_ucb_${bd_name}`);
-
-    // install
-    send_terminal(`./script/addons/install_addons_dev.sh cg_ucb_${bd_name} code_generator_${input_module.value}`);
-});
-
-document.getElementById("demo_1_btn_new_project").addEventListener("click", function () {
-    console.debug("Générer");
-    let input_module = document.getElementById("demo_1_input_module");
-    let input_model = document.getElementById("demo_1_input_model");
-    let input_champs = document.getElementById("demo_1_input_champs");
-    let input_type = document.getElementById("demo_1_input_type");
-
-    let all_good = true;
-
-    if (!input_module.value) {
-        input_module.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_module.classList.remove("invalid");
-    }
-
-    if (!input_model.value) {
-        input_model.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_model.classList.remove("invalid");
-    }
-
-    if (!input_champs.value) {
-        input_champs.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_champs.classList.remove("invalid");
-    }
-
-    if (!input_type.value) {
-        input_type.classList.add("invalid");
-        all_good = false;
-    } else {
-        input_type.classList.remove("invalid");
-    }
-
-    if (!all_good) {
-        return;
-    }
-    // let config = '{\\"model\\":[{\\"name\\":\\"b\\",\\"fields\\":[{\\"name\\":\\"b\\",\\"type\\":\\"integer\\"},{\\"name\\":\\"c\\",\\"type\\":\\"datetime\\"}]},{\\"name\\":\\"a\\",\\"fields\\":[{\\"name\\":\\"a\\",\\"type\\":\\"char\\"}]}]}'
-    let config = `{\\"model\\":[{\\"name\\":\\"${input_model.value}\\",\\"fields\\":[{\\"name\\":\\"${input_champs.value}\\",\\"type\\":\\"${input_type.value}\\"}]}]}`
-
-    // cd
-    send_terminal(`cd ${path_root}`);
-
-    // new project
-    send_terminal(`./script/code_generator/new_project.py --keep_bd_alive -m ${input_module.value} -d ${path_rel_repo} --config "${config}"`);
-});
+    return all_good;
+}
 
 // document.getElementById("demo_btn_view").addEventListener("click", function() {
 //     console.debug("visualize");
@@ -352,6 +58,193 @@ monApp.controller('FormulaireController', function ($scope) {
     //     champs: [{name: '', type: 'char'}]
     // }];
     $scope.modeles = [];
+    $scope.json = "";
+    $scope.json_inline = "";
+
+    $scope.fct_login = function () {
+        console.debug("login");
+        // login
+        send_terminal("mathben");
+        // password
+        send_terminal("testtest");
+        // clear
+        send_terminal("clear");
+    }
+
+    $scope.fct_run = function () {
+        console.debug("run");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // run
+        send_terminal("./run.sh");
+    }
+
+    $scope.fct_stop = function () {
+        console.debug("Kill all");
+        // force kill all
+        send_terminal("pkill -f odoo-bin");
+    }
+
+    $scope.fct_status = function () {
+        console.debug("status");
+        // cd
+        send_terminal(`cd ${path_repo}`);
+        // status
+        send_terminal("git status");
+    }
+
+    $scope.fct_diff = function () {
+        console.debug("diff");
+        // cd
+        send_terminal(`cd ${path_repo}`);
+        // diff
+        send_terminal("git diff");
+    }
+
+    $scope.fct_commit = function () {
+        console.debug("commit");
+        // cd
+        send_terminal(`cd ${path_repo}`);
+        // add
+        send_terminal("git add .");
+        // commit
+        send_terminal('git commit -m "test"');
+    }
+
+    $scope.fct_clean = function () {
+        console.debug("clean");
+        let all_good = validate_module();
+        // cd
+        send_terminal(`cd ${path_repo}`);
+        // clean
+        send_terminal("git clean -fd");
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // force clean if exist (or good ;-))
+        send_terminal(`rm -rf "${input_module.value}"`);
+        send_terminal(`rm -rf "code_generator_template_${input_module.value}"`);
+        send_terminal(`rm -rf "code_generator_${input_module.value}"`);
+    }
+
+    $scope.fct_new_project = function () {
+        console.debug("Générer");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // let config = '{\\"model\\":[{\\"name\\":\\"b\\",\\"fields\\":[{\\"name\\":\\"b\\",\\"type\\":\\"integer\\"},{\\"name\\":\\"c\\",\\"type\\":\\"datetime\\"}]},{\\"name\\":\\"a\\",\\"fields\\":[{\\"name\\":\\"a\\",\\"type\\":\\"char\\"}]}]}'
+        // let config = `{\\"model\\":[{\\"name\\":\\"${input_model.value}\\",\\"fields\\":[{\\"name\\":\\"${input_champs.value}\\",\\"type\\":\\"${input_type.value}\\"}]}]}`
+        let config = $scope.json_inline;
+
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // new project
+        if (config.length) {
+            send_terminal(`./script/code_generator/new_project.py --keep_bd_alive -m ${input_module.value} -d ${path_rel_repo} --config "${config}"`);
+        } else {
+            send_terminal(`./script/code_generator/new_project.py --keep_bd_alive -m ${input_module.value} -d ${path_rel_repo}`);
+        }
+    }
+
+    $scope.fct_install = function () {
+        console.debug("install");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // restore
+        send_terminal(`./script/database/db_restore.py --database ${bd_name}`);
+        // install
+        send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
+    }
+
+    $scope.fct_update = function () {
+        console.debug("update");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // update
+        send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
+    }
+
+    $scope.fct_up_uca = function () {
+        console.debug("up uca");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // restore
+        send_terminal(`./script/database/db_restore.py --database cg_uca_${bd_name}`);
+        // install
+        send_terminal(`./script/addons/install_addons_dev.sh cg_uca_${bd_name} ${input_module.value},code_generator_template_${input_module.value}`);
+    }
+
+    $scope.fct_up_ucb = function () {
+        console.debug("up ucb");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // restore
+        send_terminal(`./script/database/db_restore.py --database cg_ucb_${bd_name}`);
+        // install
+        send_terminal(`./script/addons/install_addons_dev.sh cg_ucb_${bd_name} code_generator_${input_module.value}`);
+    }
+
+    $scope.fct_stat_c = function () {
+        console.debug("stat c");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // stat
+        send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/${input_module.value}`);
+    }
+
+    $scope.fct_stat_uca = function () {
+        console.debug("stat uca");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // stat
+        send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_template_${input_module.value}`);
+    }
+
+    $scope.fct_stat_ucb = function () {
+        console.debug("stat ucb");
+        let all_good = validate_module();
+        if (!all_good) {
+            return;
+        }
+        let input_module = document.getElementById("demo_1_input_module");
+        // cd
+        send_terminal(`cd ${path_root}`);
+        // stat
+        send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_${input_module.value}`);
+    }
 
     $scope.ajouterModele = function () {
         $scope.modeles.push({
@@ -382,9 +275,9 @@ monApp.controller('FormulaireController', function ($scope) {
         $scope.modeles.forEach(function (modele) {
             let modeleData = {};
             modeleData.name = modele.name;
-            modeleData.field = [];
+            modeleData.fields = [];
             modele.champs.forEach(function (champ) {
-                modeleData.field.push({"name": champ.name, "type": champ.type});
+                modeleData.fields.push({"name": champ.name, "type": champ.type});
             });
             lst_model.push(modeleData);
         });
