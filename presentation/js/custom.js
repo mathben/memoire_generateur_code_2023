2cfab35fae2c920e462955093136802e19fe0520
Mathieu Benoit <mathben@technolibre.ca>
2023-05-02 17:41:10 -0400

    [UPD] presentation: clean html, fix json angularjs

--- presentation/js/custom.js
+++ presentation/js/custom.js
@@ -3,120 +3,80 @@
 // Source shellinabox https://github.com/shellinabox/shellinabox/blob/master/misc/embedded.html
 var url = "http://127.0.0.1:4200";
 var iframe = document.getElementById('mon-iframe');
+var path_rel_repo = "./addons/TechnoLibre_odoo-code-generator-template"
+var path_repo = "~/git/ERPLibre2/addons/TechnoLibre_odoo-code-generator-template"
+var path_root = "~/git/ERPLibre2/"
+var bd_name = "demo"
+
 // var iframe_view = document.getElementById('iframe_view');
+
+function send_terminal(str) {
+    let message = JSON.stringify({
+        type: 'input',
+        data: `${str}\n`
+    });
+    iframe.contentWindow.postMessage(message, url);
+}
+
 document.getElementById("demo_1_btn_login").addEventListener("click", function () {
     console.debug("login");
     // login
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'mathben\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("mathben");
 
     // password
-    message = JSON.stringify({
-        type: 'input',
-        data: 'testtest\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("testtest");
 
     // clear
-    message = JSON.stringify({
-        type: 'input',
-        data: 'clear\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("clear");
 });
 
 document.getElementById("demo_1_btn_run").addEventListener("click", function () {
     console.debug("run");
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // run
-    message = JSON.stringify({
-        type: 'input',
-        data: './run.sh\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("./run.sh");
 });
 
 document.getElementById("demo_1_btn_stop").addEventListener("click", function () {
     console.debug("Kill all");
     // force kill all
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'pkill -f odoo-bin\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
-
-    // iframe_view.src = ""
+    send_terminal("pkill -f odoo-bin");
 });
 
 document.getElementById("demo_1_btn_status").addEventListener("click", function () {
     console.debug("status");
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/addons/TechnoLibre_odoo-code-generator-template\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_repo}`);
 
     // status
-    message = JSON.stringify({
-        type: 'input',
-        data: 'git status\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("git status");
 });
 
 document.getElementById("demo_1_btn_diff").addEventListener("click", function () {
     console.debug("diff");
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/addons/TechnoLibre_odoo-code-generator-template\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_repo}`);
 
-    // status
-    message = JSON.stringify({
-        type: 'input',
-        data: 'git diff\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    // diff
+    send_terminal("git diff");
 });
 
 document.getElementById("demo_1_btn_commit").addEventListener("click", function () {
     console.debug("commit");
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/addons/TechnoLibre_odoo-code-generator-template\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_repo}`);
 
     // add
-    message = JSON.stringify({
-        type: 'input',
-        data: 'git add .\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("git add .");
 
     // commit
-    message = JSON.stringify({
-        type: 'input',
-        data: 'git commit -m "test"\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal('git commit -m "test"');
 });
 
 document.getElementById("demo_1_btn_clean").addEventListener("click", function () {
@@ -130,39 +90,19 @@ document.getElementById("demo_1_btn_clean").addEventListener("click", function (
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/addons/TechnoLibre_odoo-code-generator-template\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_repo}`);
 
     // clean
-    message = JSON.stringify({
-        type: 'input',
-        data: 'git clean -fd\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal("git clean -fd");
 
     if (!all_good) {
         return;
     }
 
     // force clean if exist (or good ;-))
-    message = JSON.stringify({
-        type: 'input',
-        data: `rm -rf "${input_module.value}"\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
-    message = JSON.stringify({
-        type: 'input',
-        data: `rm -rf "code_generator_template_${input_module.value}"\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
-    message = JSON.stringify({
-        type: 'input',
-        data: `rm -rf "code_generator_${input_module.value}"\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`rm -rf "${input_module.value}"`);
+    send_terminal(`rm -rf "code_generator_template_${input_module.value}"`);
+    send_terminal(`rm -rf "code_generator_${input_module.value}"`);
 });
 
 document.getElementById("demo_1_btn_stat_c").addEventListener("click", function () {
@@ -182,18 +122,10 @@ document.getElementById("demo_1_btn_stat_c").addEventListener("click", function 
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // stat
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/statistic/code_count.sh ./addons/TechnoLibre_odoo-code-generator-template/${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_stat_uca").addEventListener("click", function () {
@@ -213,18 +145,10 @@ document.getElementById("demo_1_btn_stat_uca").addEventListener("click", functio
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // stat
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/statistic/code_count.sh ./addons/TechnoLibre_odoo-code-generator-template/code_generator_template_${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_template_${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_stat_ucb").addEventListener("click", function () {
@@ -244,18 +168,10 @@ document.getElementById("demo_1_btn_stat_ucb").addEventListener("click", functio
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // stat
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/statistic/code_count.sh ./addons/TechnoLibre_odoo-code-generator-template/code_generator_${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/statistic/code_count.sh ${path_rel_repo}/code_generator_${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_install").addEventListener("click", function () {
@@ -275,25 +191,13 @@ document.getElementById("demo_1_btn_install").addEventListener("click", function
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // restore
-    message = JSON.stringify({
-        type: 'input',
-        data: './script/database/db_restore.py --database demo\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/database/db_restore.py --database ${bd_name}`);
 
     // install
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/addons/install_addons.sh demo ${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_update").addEventListener("click", function () {
@@ -313,18 +217,10 @@ document.getElementById("demo_1_btn_update").addEventListener("click", function 
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // update
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/addons/install_addons.sh demo ${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/addons/install_addons.sh ${bd_name} ${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_up_uca").addEventListener("click", function () {
@@ -344,25 +240,13 @@ document.getElementById("demo_1_btn_up_uca").addEventListener("click", function 
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // restore
-    message = JSON.stringify({
-        type: 'input',
-        data: './script/database/db_restore.py --database cg_uca_demo\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/database/db_restore.py --database cg_uca_${bd_name}`);
 
     // install
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/addons/install_addons_dev.sh cg_uca_demo ${input_module.value},code_generator_template_${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/addons/install_addons_dev.sh cg_uca_${bd_name} ${input_module.value},code_generator_template_${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_up_ucb").addEventListener("click", function () {
@@ -382,30 +266,17 @@ document.getElementById("demo_1_btn_up_ucb").addEventListener("click", function 
     }
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
     // restore
-    message = JSON.stringify({
-        type: 'input',
-        data: './script/database/db_restore.py --database cg_ucb_demo\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/database/db_restore.py --database cg_ucb_${bd_name}`);
 
     // install
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/addons/install_addons_dev.sh cg_ucb_demo code_generator_${input_module.value}\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`./script/addons/install_addons_dev.sh cg_ucb_${bd_name} code_generator_${input_module.value}`);
 });
 
 document.getElementById("demo_1_btn_new_project").addEventListener("click", function () {
     console.debug("Générer");
-    let button = document.getElementById("demo_1_btn_new_project");
     let input_module = document.getElementById("demo_1_input_module");
     let input_model = document.getElementById("demo_1_input_model");
     let input_champs = document.getElementById("demo_1_input_champs");
@@ -448,21 +319,10 @@ document.getElementById("demo_1_btn_new_project").addEventListener("click", func
     let config = `{\\"model\\":[{\\"name\\":\\"${input_model.value}\\",\\"fields\\":[{\\"name\\":\\"${input_champs.value}\\",\\"type\\":\\"${input_type.value}\\"}]}]}`
 
     // cd
-    let message = JSON.stringify({
-        type: 'input',
-        data: 'cd ~/git/ERPLibre2/\n'
-    });
-    iframe.contentWindow.postMessage(message, url);
+    send_terminal(`cd ${path_root}`);
 
-    // button.style.color = "orange";
     // new project
-    message = JSON.stringify({
-        type: 'input',
-        data: `./script/code_generator/new_project.py --keep_bd_alive -m ${input_module.value} -d ./addons/TechnoLibre_odoo-code-generator-template --config "${config}"\n`
-    });
-    iframe.contentWindow.postMessage(message, url);
-    // button.style.color = "black";
-    // iframe_view.src = ""
+    send_terminal(`./script/code_generator/new_project.py --keep_bd_alive -m ${input_module.value} -d ${path_rel_repo} --config "${config}"`);
 });
 
 // document.getElementById("demo_btn_view").addEventListener("click", function() {
@@ -487,15 +347,16 @@ iframe.src = url;
 // Gestion du formulaire
 let monApp = angular.module('monApp', []);
 monApp.controller('FormulaireController', function ($scope) {
-    $scope.modeles = [{
-        nom: '',
-        champs: [{nom: '', type: 'char'}]
-    }];
+    // $scope.modeles = [{
+    //     name: '',
+    //     champs: [{name: '', type: 'char'}]
+    // }];
+    $scope.modeles = [];
 
     $scope.ajouterModele = function () {
         $scope.modeles.push({
-            nom: '',
-            champs: [{nom: '', type: 'char'}]
+            name: '',
+            champs: [{name: '', type: 'char'}]
         });
     };
 
@@ -504,7 +365,7 @@ monApp.controller('FormulaireController', function ($scope) {
     };
 
     $scope.ajouterChamp = function (modele) {
-        modele.champs.push({nom: '', type: 'char'});
+        modele.champs.push({name: '', type: 'char'});
     };
 
     $scope.supprimerChamp = function (modele, index) {
@@ -512,17 +373,23 @@ monApp.controller('FormulaireController', function ($scope) {
     };
 
     $scope.genererJSON = function () {
-        let data = [];
+        if (!$scope.modeles.length) {
+            $scope.json = "";
+            return;
+        }
+        let lst_model = [];
+        let data = {"model": lst_model};
         $scope.modeles.forEach(function (modele) {
             let modeleData = {};
-            modeleData.modele = modele.nom;
-            modeleData.champs = {};
+            modeleData.name = modele.name;
+            modeleData.field = [];
             modele.champs.forEach(function (champ) {
-                modeleData.champs[champ.nom] = champ.type;
+                modeleData.field.push({"name": champ.name, "type": champ.type});
             });
-            data.push(modeleData);
+            lst_model.push(modeleData);
         });
         $scope.json = JSON.stringify(data, null, 2);
+        $scope.json_inline = JSON.stringify(data).replaceAll('"', '\\"');
     };
 });
 
